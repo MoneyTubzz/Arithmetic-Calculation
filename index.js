a0867e6be2271a7d18e3d64e17c9f507b62f2149
Malek Eltassi <eltassimalek@gmail.com>
2025-03-03 22:36:41 -0700

    finished the assignment

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
-const {add, sub, div, mul} = require('./arithmeticFunctions');
+const arithmeticFunctions = require('./arithmeticFunctions');
 
+app.use(express.urlencoded({ extended: true }));
 
-//local Server
+//3.) set up local Server
 app.listen(2025, function(){
-console.log("Server online");
+console.log("Server online - Port #2025");
 });
 
-app.post('/calculate', (req,res) => {
-  
 
+//Post Route
+app.post('/calculate', (req,res) => {
+
+    const { operation } = req.body;
+    let value_1 = parseFloat(req.body.value_1);
+    let value_2 = parseFloat(req.body.value_2);
+
+    const operations = {
+        add: "Addition",
+        sub: "Subtraction",
+        mul: "Multiplication",
+        div: "Division"
+    };
+
+    const result = arithmeticFunctions[operation](value_1,value_2);
+
+    //Print results to console
+    console.log(`Operation: ${operations[operation]}.`);
+    console.log(`Value 1: ${value_1}`);
+    console.log(`Value 2: ${value_2}`);
+    console.log(`Result: ${result}`);
+    
+    //send result back to Postman (as plain text)
+    res.status(200).send(`Operation: ${operations[operation]}.\nValue 1: ${value_1}\nValue 2: ${value_2}\nResult: ${result}`);
 });
